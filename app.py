f424238c32ee2ae948457d94bfd73ca1fd91260f
valeriapopova <iamvaleriapopova@yandex.ru>
2022-08-29 19:38:07 +0400

    local

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, Response, url_for
+from werkzeug.exceptions import BadRequestKeyError
+
+from config import Configuration
+from forms import FileForm
+
+
+from xls import get_data_key, get_data_value, clear_and_append, create_xls, create_worksheet
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    if request.method == 'POST':
+        try:
+            json_file = request.form['file']
+            if json_file.endswith('.json'):
+                data_keys = get_data_key(json_file)
+                data_values = get_data_value(json_file)
+                workb = create_xls(json_file)
+                worksh = create_worksheet(workb)
+
+                clear_and_append(worksh, data_keys, data_values)
+                workb.close()
+                return redirect(url_for('result_page'))
+            else:
+                return Response("Недопустимый формат файла, необходимое расширение - JSON", 404)
+        except BadRequestKeyError:
+            return Response("Пустое значение", 400)
+
+    form = FileForm()
+    return render_template('homepage.html', form=form), 200
+
+
+@app.route('/result')
+def result_page():
+    return render_template('resultpage.html'), 201
---
+++ xls.py
@@ -0,0 +1,58 @@
+import xlsxwriter
+import os.path
+import json
+from flask import Response
+
+
+def create_xls(json_file):
+    name, __ = os.path.splitext(json_file)
+
+    workbook = xlsxwriter.Workbook(f'{name}.xlsx')
+    # workbook = xlsxwriter.Workbook(f'1.xlsx')
+    return workbook
+
+
+def create_worksheet(workbook):
+    worksheet = workbook.add_worksheet()
+    return worksheet
+
+
+def get_data_key(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        try:
+            result = data['data']
+            data_for_sheets = []
+            for key in result:
+                for k in key:
+                    data_for_sheets.append(k)
+            return data_for_sheets
+        except KeyError:
+            return Response("Данные для записи не найдены", 404)
+
+
+def get_data_value(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        try:
+            result = data['data']
+            data_for_sheets = []
+            for res in result:
+                for k, v in res.items():
+                    data_for_sheets.append(v)
+            return data_for_sheets
+        except KeyError:
+            return Response("Данные для записи не найдены", 404)
+
+
+def clear_and_append(worksheet, data_keys, data_values):
+    col = 0
+    for k in data_keys:
+        worksheet.write(0, col, k)
+        col += 1
+    col = 0
+    for k, v in enumerate(data_values):
+        worksheet.write_column(1, col, v)
+        col += 1
+
+
